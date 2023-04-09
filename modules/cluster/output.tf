e57c9f246120092954b786cd71c4be12cffdec9b
Bjarne Meyn <UI930057@rwe-ag.com>
2023-04-09 19:40:26 +0200

    added outputs to cluster module

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -1,3 +1,9 @@
 output "name" {
   value = azurerm_kusto_cluster.this.name
 }
+output "cluster_uri" {
+  value = azurerm_kusto_cluster.this.cluster_uri
+}
+output "data_ingestion_uri" {
+  value = azurerm_kusto_cluster.this.data_ingestion_uri
+}
