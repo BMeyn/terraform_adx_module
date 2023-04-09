92e913a0f525262594673fba8cb2b1327a2b71f8
Bjarne Meyn <UI930057@rwe-ag.com>
2023-04-09 19:39:58 +0200

    added outputs to main module

---
+++ outputs.tf
@@ -0,0 +1,13 @@
+
+output "cluster_name" {
+  value = module.cluster.name
+}
+ 
+output "cluster_uri" {
+  value = module.cluster.cluster_uri
+}
+
+output "data_ingestion_uri" {
+  value = module.cluster.data_ingestion_uri
+}
+
