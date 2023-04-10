b6937305864027aec2580d4e8de1795974faa8d3
Bjarne Meyn <UI930057@rwe-ag.com>
2023-04-10 07:24:02 +0000

    debug wrong cluster uri reference in cluster module

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -2,7 +2,7 @@ output "name" {
   value = azurerm_kusto_cluster.this.name
 }
 output "cluster_uri" {
-  value = azurerm_kusto_cluster.this.cluster_uri
+  value = azurerm_kusto_cluster.this.uri
 }
 output "data_ingestion_uri" {
   value = azurerm_kusto_cluster.this.data_ingestion_uri
